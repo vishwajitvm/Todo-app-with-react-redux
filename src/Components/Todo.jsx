64ae3bf0b41bbb2edd1621efa6af9ca7acd06cab
vishwajitvm <vishwajitmall50@gmail.com>
2023-07-23 21:27:31 +0530

    Add , delete , clear all - functionality

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,27 +1,48 @@
 import React, { useState } from 'react'
-import './todo.css' ;
+import './todo.css';
 import { addToDo, deleteToDo, removeToDo } from '../Actions';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Todo = () => {
-    const dispatch = useDispatch() ;
-    const [inputData , setInputData] = useState("")
-  return (
-    <>
-      <div className='main-div'>
-        <div className='child-div'>
-          <figure>
-            <figcaption> Add you list here </figcaption>
-          </figure>
+    const dispatch = useDispatch();
+    const list = useSelector((state) => state.todoReducers.list);
+    const [inputData, setInputData] = useState("")
+    return (
+        <>
+            <div className='main-div'>
+                <div className='child-div'>
+                    <figure>
+                        <figcaption> Add you list here </figcaption>
+                    </figure>
 
-          <div className='addItems'>
-            <input type="text" placeholder='✍️ Add items...' value={inputData} onChange={(event) => setInputData(event.target.value) } />
-            <i className='fa fa-plus add-btn' onClick={ () => dispatch(addToDo(inputData) , setInputData('')) } ></i>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+                    <div className='addItems'>
+                        <input type="text" placeholder='✍️ Add items...' value={inputData} onChange={(event) => setInputData(event.target.value)} />
+                        <i className='fa fa-plus add-btn' onClick={() => dispatch(addToDo(inputData), setInputData(''))} ></i>
+                    </div>
+
+                    <div className='showItems'>
+                        {
+                            list.map((elem) => {
+                                return (
+                                    <div className="eachItem" key={elem.id}>
+                                        <h3> {elem.data} </h3>
+                                        <div className="todo-btn">
+                                            <i className='far fa-trash-alt add-btn' title='Delete item' onClick={() => dispatch(deleteToDo(elem.id))} ></i>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
+
+                    <div className='shshowItemsshowItemsow'>
+                        <button className='btn effect04' data-sm-link-text="remove All" onClick={ () => dispatch(removeToDo()) }> <span> Check list </span> </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Todo
--- src/reducers/todoReducers.js
+++ src/reducers/todoReducers.js
@@ -1,22 +1,36 @@
 const initialData = {
-    list: []
-}
-const todoReducers  = (state=initialData, action) => {
-    switch ( action.type ) {
-        case "ADD_TODO":
-            const { id , data } = action.payload ;
-            return {
-                ...state ,
-                list:[
-                    ...state.list,
-                    {
-                        id: id,
-                        data: data
-                    }
-                ]
-            }
-            default: return state
-    }
-}
+  list: [],
+};
+const todoReducers = (state = initialData, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      const { id, data } = action.payload;
+      return {
+        ...state,
+        list: [
+          ...state.list,
+          {
+            id: id,
+            data: data,
+          },
+        ],
+      };
 
-export default todoReducers ;
+      case "DELETE_TODO":
+      const newlist = state.list.filter( (elem) => elem.id !== action.id )
+      return {
+        ...state,
+        list: newlist
+      };
+
+      case "REMOVE_TODO":
+      return {
+        ...state,
+        list: []
+      };
+    default:
+      return state;
+  }
+};
+
+export default todoReducers;
