30e51fb6bfb005ce3f62d923b545dfd96f401e84
vishwajitvm <vishwajitmall50@gmail.com>
2023-07-23 19:16:10 +0530

    added all my required basic actions

---
+++ src/Actions/index.js
@@ -0,0 +1,17 @@
+export const addToDo = () => {
+    return{
+        type: "ADD_TODO"
+    }
+}
+
+export const deleteToDo = () => {
+    return{
+        type: "DELETE_TODO"
+    }
+}
+
+export const removeToDo = () => {
+    return{
+        type: "REMOVE_TODO"
+    }
+}
